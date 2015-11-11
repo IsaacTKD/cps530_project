b287bf709988c3f78a143f98824a7183225fd3c9
IsaacTKD <mhtong22@hotmail.com>
2015-11-11 18:08:19 -0500

    CPS530_project

---
+++ views/site/about.php
@@ -0,0 +1,60 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About us';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<h1><?= Html::encode($this->title) ?></h1>
+
+<div class="site-about">
+
+    <div class="list-group">
+      <a href="#" class="list-group-item active">
+        <h4 class="list-heading">Team members</h4>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Isaac Tong</h4>
+        <ul>
+          <li class="list-group-item-text">University: Ryerson</li>
+          <li class="list-group-item-text">Program: Computer Science</li>
+          <li class="list-group-item-text">Year(s) of programming: 0</li>
+          <li class="list-group-item-text">Responsibilities: Buy coffee for Dr Shiran</li>
+        </ul>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Charlie Driver</h4>
+        <ul>
+          <li class="list-group-item-text">University: Ryerson</li>
+          <li class="list-group-item-text">Program: Computer Science</li>
+          <li class="list-group-item-text">Year(s) of programming: 5</li>
+          <li class="list-group-item-text">Responsibilities: Buy food for Dr Shiran</li>
+        </ul>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Dr. Matin Shirandasht</h4>
+        <ul>
+          <li class="list-group-item-text">University: Massachusetts Institute of Technology</li>
+          <li class="list-group-item-text">Program: Master of Computer Science</li>
+          <li class="list-group-item-text">Year(s) of programming: 10 </li>
+          <li class="list-group-item-text">Responsibilities: In charge of the whole project</li>
+        </ul>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Eric Karnis</h4>
+        <ul>
+          <li class="list-group-item-text">University: Ryerson</li>
+          <li class="list-group-item-text">Program: Computer Science</li>
+          <li class="list-group-item-text">Year(s) of programming: 5</li>
+          <li class="list-group-item-text">Responsibilities: Find girls for Dr Shiran </li>
+        </ul>
+      </a>
+</div>
+    <p>
+        References :
+    </p>
+
+
+</div>
---
+++ views/site/index.php
@@ -0,0 +1,85 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Yii Framework';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>The presentation of Yii Framework!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Popularity</h2>
+                <script type="text/javascript" src="//www.google.ca/trends/embed.js?hl=en-US&q=Yii,+/m/09t3sp,+Symfony,+/m/07h2gf,+WebGUI&cmpt=q&tz=Etc/GMT%2B5&tz=Etc/GMT%2B5&content=1&cid=TIMESERIES_GRAPH_AVERAGES_CHART&export=5&w=1000&h=350"></script>
+
+            </div>
+
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+              <h2>Technological bases</h2>
+
+              <p>Fill this in</p>
+
+          </div>
+        </div>
+        <div class="row">
+
+            <div class="col-lg-6">
+                <h2>Strengths</h2>
+                  <ul>
+                    <li>Easy to Install</li>
+                    <li>Model-View-Controller</li>
+                    <li>Easy to debug the code</li>
+                  </ul>
+
+            </div>
+            <div class="col-lg-6">
+                <h2>Weaknesses</h2>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="container">
+                  <h2>Which developers mostly use and for what kind of websites</h2>
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>Developers</th>
+                        <th>Websites (kind)</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Web designer</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>Web programmer</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>Flash developer</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>Content developer</td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+
+            </div>
+        </div>
+</div>
