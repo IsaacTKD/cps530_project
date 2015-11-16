eeb024bd2c500bdb588917e95c25245fc6a43cc7
IsaacTKD <mhtong22@hotmail.com>
2015-11-16 00:42:21 -0500

    changed the db password

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=database',
     'username' => 'root',
-    'password' => '',
+    'password' => 'password',
     'charset' => 'utf8',
 ];
