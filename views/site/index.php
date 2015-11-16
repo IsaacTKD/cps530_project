b88f7853d7127b0c66a0ceb8c5f90d18710c78a3
ekarnis <ekarnis@outlook.com>
2015-11-15 23:00:12 -0500

    Expanded on MVC, added diagram, small changes elsewhere

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,7 @@ $this->title = 'Yii Framework';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>The presentation of Yii Framework!</h1>
-
-        <!-- <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
+        <h1>The Yii Framework!</h1>
     </div>
 
     <div class="body-content">
@@ -19,8 +15,7 @@ $this->title = 'Yii Framework';
         <div class="col-lg-12">
             <h2>What Is Yii?</h2>
             <p>
-            Yii (pronounced "Yee") is a PHP-based open source web application framework. Yii promotes DRY (Don't Repeat Yourself) design and rapid development.
-            A main focus of the Yii framework is performance and efficiency, with Yii outperforming all other PHP-based frameworks in the field of requests processed per second.
+            Yii (pronounced "Yee") is a PHP-based open source web application framework. Yii stands for "Yes, it is!". This comes from their slogan: "Is it fast?...Is it secure?...Is it professional?...Is it right for my next project?...Yes, it is!" Yii promotes DRY (Don't Repeat Yourself) design and rapid development. A main focus of the Yii framework is performance and efficiency, with Yii outperforming all other PHP-based frameworks in the field of requests processed per second.
             </p>
               <figure class="center">
                   <img src="performance-20090131.png" alt="Performance Comparison Among PHP Frameworks" height="300" width="550"/>
@@ -33,7 +28,7 @@ $this->title = 'Yii Framework';
         <div class="row">
             <div class="col-lg-12">
                 <h2>Popularity</h2>
-                <script type="text/javascript" src="//www.google.ca/trends/embed.js?hl=en-US&q=Yii,+/m/09t3sp,+Symfony,+/m/07h2gf,+WebGUI&cmpt=q&tz=Etc/GMT%2B5&tz=Etc/GMT%2B5&content=1&cid=TIMESERIES_GRAPH_AVERAGES_CHART&export=5&w=1000&h=350"></script>
+                <script type="text/javascript" src="//www.google.ca/trends/embed.js?hl=en-US&q=Yii,+/m/09t3sp,+Symfony,+/m/07h2gf,+WebGUI&cmpt=q&tz=Etc/GMT%2B5&tz=Etc/GMT%2B5&tz=Etc/GMT%2B5&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=1000&h=330"></script>
 
             </div>
 
@@ -47,11 +42,17 @@ $this->title = 'Yii Framework';
                 The MVC design pattern separates the application rules and logic from the user interface, so each part can be edited without
                 affecting the others.
                 <ul>
-                  <li>The Model component of MVC contains the behaviour of the application, and is independant of the user interface. The model manages application rules, logic, and data.</li>
-                  <li>The View component is the visual output of information, based on any changes made to the model.</li>
-                  <li>The Controller component sends commands to the model to update/change its state. It can also send commands to a view, to alter its output.</li>
+                  <li>The Model component of MVC contains the data used by the application and the rules that affect that data.</li>
+                  <li>The View component is the visual output of information. This is the part of the application that the user sees and interacts with directly.</li>
+                  <li>The Controller component is the brains of the application. It controls all the computation that the application does, including user interactions, modifying the model, and updating the view.</li>
                 </ul>
               </p>
+              <figure class="center">
+                  <img src="MVC.png" alt="Model-View-Controller Relationship" height="500" width="500"/>
+                  <figcaption>
+                    Model-View-Controller Relationship
+                  </figcaption>
+              </figure>
 
           </div>
         </div>
