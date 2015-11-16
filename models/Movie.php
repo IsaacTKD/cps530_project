28182559b7f78d67b64ba7cdcae668b249df7cc9
IsaacTKD <mhtong22@hotmail.com>
2015-11-15 20:12:48 -0500

    Movie database

---
+++ models/Movie.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Movie".
+ *
+ * @property integer $movie_id
+ * @property string $movie_name
+ * @property string $movie_runningtime
+ * @property integer $movie_year
+ * @property string $movie_director
+ * @property string $movie_genre
+ */
+class Movie extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'Movie';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['movie_name', 'movie_runningtime', 'movie_year', 'movie_director', 'movie_genre'], 'required'],
+            [['movie_year'], 'integer'],
+            [['movie_name', 'movie_director', 'movie_genre'], 'string', 'max' => 50],
+            [['movie_runningtime'], 'string', 'max' => 10]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'movie_id' => 'Movie ID',
+            'movie_name' => 'Movie Name',
+            'movie_runningtime' => 'Movie Runningtime',
+            'movie_year' => 'Movie Year',
+            'movie_director' => 'Movie Director',
+            'movie_genre' => 'Movie Genre',
+        ];
+    }
+}
