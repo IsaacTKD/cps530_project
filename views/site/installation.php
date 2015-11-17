84f0707f81898a33d24cdcbe25e048f72a193267
Eric Karnis <ekarnis@outlook.com>
2015-11-16 21:50:32 -0500

    did some formatting

--- views/site/installation.php
+++ views/site/installation.php
@@ -20,78 +20,34 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="body-content"  id="body-content">
       <div class="row">
         <div class="col-lg-6">
-            <h2>Requirements:</h2>
-            <ul>
-              <li>WampServer</li>
-              <li>Composer</li>
+          <h2>Requirements:</h2>
+          <ul>
+            <li>WampServer</li>
+            <li>Composer</li>
+          </ul>
+          <h2>Install WampServer:</h2>
+          <figure class="left">
+            <img src="Source Forge Wamp.png" alt="Install Wamp" height="400" width="800" />
+            <figcaption>Wamp Installation file from Source Forge</figcaption>
+          </figure>
 
-            </ul>
-            <div class="row">
-                <div class="col-lg-6" >
-                    <h2>Install WampServer:</h2>
-                      <br/>
-
-                </div>
-
-            </div>
-
-              <figure class="left">
-                  <img src="Source Forge Wamp.png" alt="Install Wamp" height="400" width="800" />
-                  <figcaption>Wamp Installation file from Source Forge</figcaption>
-
-              </figure>
-
+          <h2>Install  Composer from
+            <a href="http://www.yiiframework.com/doc-2.0/guide-start-installation.html">here</a>
+          </h2>
+          <img src="Install Yii.png" alt="Install Wamp" height="400" width="800" />
+          <h2>For Windows, download and run the installer</h2>
+          <img src="InstallLink.png" alt="Install Wamp" height="400" width="800" />
+          <h2>For Linux and Mac OS X, run the provided commands in command prompt</h2>
+          <img src="cmd.png" alt="Install Wamp" height="400" width="800" />
+          <h2>Update composer if the first commmand did not execute</h2>
+          <img src="updatecomposer.png" alt="Install Wamp" height="200" width="1000" />
+          <h2>Create first application: </h2>
+          <img src="second command.png" alt="Install Wamp" height="500" width="1000" />
+          <h2>Go to : localhost/web/index.php: </h2>
+          <img src="start-app-installed.png" alt="Install Wamp" height="650" width="1000" />
+          
         </div>
       </div>
-        <div class="row">
-            <div class="col-lg-10">
-                <h2>Install  Composer from
-                  <a href="http://www.yiiframework.com/doc-2.0/guide-start-installation.html">here</a>
-                </h2>
-                <img src="Install Yii.png" alt="Install Wamp" height="400" width="800" />
-                <br/>
-                <br/>
-
-                <h2>For Windows, download and run the installer</h2>
-                <img src="InstallLink.png" alt="Install Wamp" height="400" width="800" />
-            </div>
-
-        </div>
-        <div class="row">
-          <div class="col-lg-6">
-              <h2>For Linux and Mac OS X, run the provided commands in command prompt</h2>
-
-            <img src="cmd.png" alt="Install Wamp" height="400" width="800" />
-
-          </div>
-        </div>
-        <div class="row">
-
-            <div class="col-lg-6">
-                <h2>Update composer if the first commmand did not execute</h2>
-                <img src="updatecomposer.png" alt="Install Wamp" height="200" width="1000" />
-            </div>
-
-        </div>
-        <div class="row">
-            <div class="col-lg-6">
-
-                  <h2>Create first application: </h2>
-
-                  <img src="second command.png" alt="Install Wamp" height="500" width="1000" />
-
-
-            </div>
-        </div>
-
-
-        <div class="row">
-            <div class="col-lg-6">
-
-                  <h2>Go to : localhost/web/index.php: </h2>
-                  <img src="start-app-installed.png" alt="Install Wamp" height="650" width="1000" />
-
-
-            </div>
-        </div>
+    </div>
+  </section>
 </div>
